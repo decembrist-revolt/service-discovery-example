48a5fd40801c80c46e1bf542b35b363ab73dd249
decembrist-revolt <Dkhtfrth1github>
2021-04-24 20:39:17 +0300

    init commit

---
+++ src/main/java/org/decembrist/discovery/SpringDiscoveryExampleApplication.java
@@ -0,0 +1,13 @@
+package org.decembrist.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiscoveryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiscoveryExampleApplication.class, args);
+    }
+
+}
