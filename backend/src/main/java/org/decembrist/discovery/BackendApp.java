526f227fad97842172f4bff067be2f5b9b5e2edd
decembrist-revolt <Dkhtfrth1github>
2021-04-24 20:53:10 +0300

    backend pong controller

---
+++ backend/src/main/java/org/decembrist/discovery/BackendApp.java
@@ -0,0 +1,25 @@
+package org.decembrist.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackendApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApp.class);
+    }
+
+}
+
+@RestController
+class BackendLogicController {
+
+    @GetMapping("/ping")
+    public String pong() {
+        return "pong";
+    }
+
+}
