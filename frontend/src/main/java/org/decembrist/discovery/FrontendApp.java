c0b6ad78f5c553fb5abe8cb3f443df038325e82d
decembrist-revolt <Dkhtfrth1github>
2021-04-24 22:49:15 +0300

    load balancer client

--- frontend/src/main/java/org/decembrist/discovery/FrontendApp.java
+++ frontend/src/main/java/org/decembrist/discovery/FrontendApp.java
@@ -2,8 +2,8 @@ package org.decembrist.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URI;
 
 @SpringBootApplication
@@ -36,21 +37,27 @@ class FrontendConfig {
 @RestController
 class FrontendController {
 
+    public static final String BACKEND_SERVICE_ID = "backend";
+
     private final RestTemplate restTemplate;
     private final DiscoveryClient client;
+    private final LoadBalancerClient loadBalancer;
 
     public FrontendController(RestTemplate restTemplate,
-                              DiscoveryClient client) {
+                              DiscoveryClient client,
+                              LoadBalancerClient loadBalancer) {
         this.restTemplate = restTemplate;
         this.client = client;
+        this.loadBalancer = loadBalancer;
     }
 
     @GetMapping("/backend/ping")
-    public String pong() {
-        ServiceInstance backendInstance = client.getInstances("backend").get(0);
-        URI backendUrl = backendInstance.getUri().resolve("/ping");
-        String pong = restTemplate.getForObject(backendUrl, String.class);
-        return pong + " " + backendInstance.getInstanceId();
+    public String pong() throws IOException {
+        return loadBalancer.execute(BACKEND_SERVICE_ID, backendInstance -> {
+            URI backendUrl = backendInstance.getUri().resolve("/ping");
+            String pong = restTemplate.getForObject(backendUrl, String.class);
+            return pong + " " + backendInstance.getInstanceId();
+        });
     }
 
 }
