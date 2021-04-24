b3a610a7d0b63404ae083bc0a3bb4643a1bf8024
decembrist-revolt <Dkhtfrth1github>
2021-04-24 21:17:38 +0300

    disable discovery service self errors subscribe backend as eureka client

--- backend/src/main/java/org/decembrist/discovery/BackendApp.java
+++ backend/src/main/java/org/decembrist/discovery/BackendApp.java
@@ -2,10 +2,12 @@ package org.decembrist.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BackendApp {
 
     public static void main(String[] args) {
